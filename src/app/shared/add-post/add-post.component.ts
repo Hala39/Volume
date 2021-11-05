e83fcc30ab7b71a517a0d6beff7fb2782a3e3943
F&H <laphala.11@gmail.com>
2021-11-05 23:27:54 +0200

    add post responsivity

--- src/app/shared/add-post/add-post.component.ts
+++ src/app/shared/add-post/add-post.component.ts
@@ -69,6 +69,9 @@ export class AddPostComponent implements OnInit {
 
   // Dialog
   displayDialogFunc() {
+    this.editorToggle = false;
+    this.photoUploadToggle = false;
+    this.fileUploadToggle = false;
     this.displayDialog = true;
   }
 
